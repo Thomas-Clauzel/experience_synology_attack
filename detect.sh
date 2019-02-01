4d9ef1691ee272ad8b893dc49f8c0096b8873a1e
Thomas Clauzel <t.clauzel@gmail.com>
2019-02-01 16:59:10 +0100

    Update detect.sh

--- detect.sh
+++ detect.sh
@@ -1,9 +1,7 @@
  #!/bin/bash
-# Program name: pingall.sh
 date
 cat list.txt |  while read output
 do
-#    curl http://192.168.1.101:5000/webman/index.html > index.html
     curl --max-time 15  --connect-timeout 15 "http://$output:5000/index.html" > index.html
     var=$(grep synology index.html)
     if [[ $var =~ synology ]]
